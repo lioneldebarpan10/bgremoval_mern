a824aa94ab61d56bc9e06caf48d86469c62ca7a9
Debarpan Deb <dbrpn07cse@gmail.com>
2026-01-15 03:05:50 +0530

    First Commit, Creating Navbar and Header

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+   return (
+      <div className = 'flex items-center justify-between mx-4 py-3 lg:mx-44'>
+         <Link to = '/'> <img src= {assets.logo} alt="logo" className='w-32 sm:w-44'/> </Link>
+         <button className='bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full cursor-pointer'>
+            Get Started <img src= {assets.arrow_icon} alt="arrow-icon" className='w-3 sm:w-4'/>
+         </button>
+      </div>
+
+   )
+}
+
+export default Navbar
